618dc6a3d71715e07f9c08c9d1f6fa93aedbb060
Test46Jenkins <158430604+Test46Jenkins@users.noreply.github.com>
2024-07-26 16:22:05 +0530

    Update aws_codedeploy.sh

--- scripts/aws_codedeploy.sh
+++ scripts/aws_codedeploy.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-# Check if required variables are set
-if [ -z "$codedeploy_application_name" ] || [ -z "$codedeploy_groupname" ] || [ -z "$aws_s3_bucket_name" ]; then
-  echo "Error: Required environment variables are not set."
-  exit 1
+
+
+deploy_id=$(aws deploy create-deployment --application-name $codedeploy_application_name --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name $codedeploy_groupname --s3-location bucket=$aws_s3_bucket_name,bundleType=zip,key=cicd-codedeploy/$CI_PIPELINE_ID.zip --region us-east-2 --output text)
+
+while :
+    do
+        deploystatus=$(aws deploy get-deployment --deployment-id $deploy_id --query "deploymentInfo.[status]"  --region us-east-2 --output text)
+        if [ "$deploystatus" = "Succeeded" ]
+           then
+            echo "Deployment $deploy_id is now $deploystatus"
+            echo ""
+            break
+        elif [ "$deploystatus" = "Failed" ]
+            then
+            echo "Deployment $deploy_id is now $deploystatus"
+            exit 1
+        else
+        echo "Deployment $deploy_id is now $deploystatus"
+        fi
+        sleep 30
+    done
+
+overalldeploystatus=$(aws deploy list-deployment-instances --deployment-id $deploy_id --instance-status-filter "Failed" --region us-east-2 --output text)
+
+if [ -z "$overalldeploystatus" ]
+    then
+        echo "Deploy to all instances are Successful"
+    else
+        echo "Deploy to some instances are Failed"
+        exit 1
 fi
-
-# Create a deployment
-deployment_id=$(aws deploy create-deployment \
-    --application-name "$codedeploy_application_name" \
-    --deployment-group-name "$codedeploy_groupname" \
-    --s3-location bucket="$aws_s3_bucket_name",bundleType=zip,key=cicd-codedeploy/${GITHUB_RUN_ID}.zip \
-    --output text --query 'deploymentId')
-
-# Check if the deployment ID was retrieved successfully
-if [ -z "$deployment_id" ]; then
-  echo "Error: Failed to create deployment."
-  exit 1
-fi
-
-echo "Deployment created with ID: $deployment_id"
-echo $deployment_id > deployment_id.txt
-
-# Wait for the deployment to complete
-aws deploy wait deployment-successful --deployment-id "$deployment_id"
